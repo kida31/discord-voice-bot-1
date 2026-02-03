b6eeec1cd220e3c90c301a30689175628d627eac
mattiTD <matthias.nguyen@service-nguyen.de>
2026-02-03 21:23:30 +0100

    fix voice-state-update

--- src/events/voice-state-update.ts
+++ src/events/voice-state-update.ts
@@ -1,4 +1,4 @@
-/* import * as GuildVCAnnouncer from "@lib/tts/GuildVoiceChannelAnnouncer";
+import * as GuildVCAnnouncer from "@lib/tts/GuildVoiceChannelAnnouncer";
 import {
   memberSelfMuted,
   memberSelfUnmuted,
@@ -16,12 +16,12 @@ export default async function voiceStateUpdateListener(
   if (oldState.selfMute !== newState.selfMute) {
     if (newState.selfMute) {
       // User hat sich gemutet
-      console.log(`${newState.member?.displayName} hat sich gemutet.`);
+      // console.log(`${newState.member?.displayName} hat sich gemutet.`);
       await announceSelfMuteChange(newState, true);
     } else {
       // User hat sich entmutet
       console.log(`${newState.member?.displayName} hat sich entmutet.`);
-      await announceSelfMuteChange(newState, false);
+      // await announceSelfMuteChange(newState, false);
     }
   }
 
@@ -48,4 +48,4 @@ async function announceSelfMuteChange(
   announcer.languageCode =
     GuildVCAnnouncer.getGuildVoiceLanguage(state.guild.id);
   await announcer.play(message);
-} */
+}
