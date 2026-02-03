a36bd47115de41d84c0fe140990e0898b2c41a81
mattiTD <matthias.nguyen@service-nguyen.de>
2026-02-03 19:32:08 +0100

    add japanse voice join/left

--- src/lib/tts/announcer-phrases.ts
+++ src/lib/tts/announcer-phrases.ts
@@ -8,6 +8,8 @@ export function memberLeftChannel(name: string, language: LanguageCode = "en") {
       return `${name} left your channel.`;
     case "de-DE":
       return `${name} hat den Channel verlassen.`;
+    case "ja-JP":
+      return `${name} チャンネルを離れました。`;
     default:
       console.warn("Language code not implemented:", language);
       return memberLeftChannel(name, "en");
@@ -25,6 +27,8 @@ export function memberJoinedChannel(
       return `${name} đã tham gia channel của bạn.`;
     case "de-DE":
       return `${name} ist dem Channel beigetreten.`;
+    case "ja-JP":
+      return `${name} チャンネルに参加しました。`;
     default:
       console.warn("Language code not implemented:", language);
       return memberJoinedChannel(name, "en");
