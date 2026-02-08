993dfe1e9691afef377327ee3ca90229b6e51c66
mattiTD <matthias.nguyen@service-nguyen.de>
2026-02-08 21:42:21 +0100

    change voice style

--- src/lib/tts/audio-provider/ElevenLabsProvider.ts
+++ src/lib/tts/audio-provider/ElevenLabsProvider.ts
@@ -15,7 +15,7 @@ type ElevenVoiceSettings = {
    ======================================================= */
 
 const VOICE_BY_LANGUAGE: Record<string, string> = {
-  "en-US": "qxTFXDYbGcR8GaHSjczg",
+  "en-US": "qxTFXDYbGcR8GaHSjczg", // English male voice (qxTFXDYbGcR8GaHSjczg) / English female voice (eVItLK1UvXctxuaRV2Oq) 
   "en": "qxTFXDYbGcR8GaHSjczg",
   "de-DE": "iwP1PxYYSTdHA1qXlwFe",
   "de": "iwP1PxYYSTdHA1qXlwFe",
@@ -74,9 +74,9 @@ export class ElevenLabsProvider implements TTSService {
     language: "en-US",
     model: "eleven_turbo_v2_5", //alternative "eleven_multilingual_v2" but VN not working with it 
     voiceId: DEFAULT_VOICE_ID,
-    stability: 0.4, // 0.0 (more random) bis 1.0 (more stable)
-    similarityBoost: 0.65, // 0.0 (less similar) bis 1.0 (more similar)
-    style: 0.3, // 0.0 (neutral) bis 1.0 (more style, e.g. emotional)
+    stability: 0.0, // 0.0 (more random) bis 1.0 (more stable)
+    similarityBoost: 0.3, // 0.0 (less similar) bis 1.0 (more similar)
+    style: 0.9, // 0.0 (neutral) bis 1.0 (more style, e.g. emotional)
     speakerBoost: true,
     outputFormat: "opus_48000_128",
   };
