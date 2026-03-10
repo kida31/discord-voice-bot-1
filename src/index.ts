d266a7efc5470f2fbf9b00e67d87f8e86bfb8a19
kienda <83455653+kida31@users.noreply.github.com>
2026-03-10 13:24:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,27 @@ import errorListener from "@events/error";
 function main() {
     // Initialize TTS announcer
     subscribeToGuild(devGuildId);
-    // Connect with database
 
     // Create a new client instance
     const client = new Client(clientOptions);
 
-    // When the client is ready, run this code (only once)
-    client.once("clientReady", async () => {
-        console.log(`\nReady! Logged in as ${client.user?.tag}\n`);
-    });
-
-    // Login to Discord with your client's token
-    client.login(token);
+    client
+        // When the client is ready, run this code (only once)
+        .once("clientReady", async () => {
+            console.log(`\nReady! Logged in as ${client.user?.tag}\n`);
+        })
+        // Error Handling
+        .on(Events.Error, errorListener)
+        // Mostly slash command handling
+        .on(Events.InteractionCreate, interactionCreateListener)
+        // Handle user join/leave voice channel
+        .on(Events.VoiceStateUpdate, voiceStateUpdateListener)
+        // Read messages in channels
+        .on(Events.MessageCreate, messageCreateListener);
 
     client
-        .on(Events.Error, errorListener)
-        .on(Events.InteractionCreate, interactionCreateListener)
-        .on(Events.VoiceStateUpdate, voiceStateUpdateListener)
-        .on(Events.MessageCreate, messageCreateListener);
+        .login(token) // Login to Discord with your client's token
+        .then(s => console.log("[login] " + s));
 }
 
 main();
